1759f4047c2544b0ce782c497d1c9e572e0c54e9
Harry Alexander Xin <harryxinfirefly919@gmail.com>
2026-08-01 11:15:11 -0400

    fix: support Vercel Upstash environment names

--- api/rlcard/status.js
+++ api/rlcard/status.js
@@ -1,7 +1,14 @@
 import { Redis } from "@upstash/redis";
 import { createStatusHandler } from "../../lib/rlcard-api.js";
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url:
+    process.env.UPSTASH_REDIS_REST_URL ??
+    process.env.KV_REST_API_URL,
+  token:
+    process.env.UPSTASH_REDIS_REST_TOKEN ??
+    process.env.KV_REST_API_TOKEN,
+});
 const store = {
   get: (key) => redis.get(key),
   set: (key, value) => redis.set(key, value),
